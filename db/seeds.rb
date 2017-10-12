de077f7ef768e8088dfeaec147fe30234782d9f8
Debbie Jong <surrealstardust@gmail.com>
2017-10-12 10:22:49 +0900

    add seed of ingredients

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,44 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Ingredient.create(name: "lemon")
+# Ingredient.create(name: "ice")
+# Ingredient.create(name: "mint leaves")
+
+# require 'open-uri'
+# require 'json'
+
+# url = 'http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+# ingredient_serialized = open(url).read
+# ingredient = JSON.parse(ingredient_serialized)
+
+# # Ingredient.create()
+# puts ingredient
+
+
+puts 'Cleaning database...'
+Ingredient.destroy_all
+
+puts 'Creating ingredients...'
+# ingredients = [
+#   {
+#     name:         "lemon"
+#   },
+#   {
+#     name:         "ice"
+#   },
+#   {
+#     name:         "mint leaves"
+#   }
+# ]
+# Ingredient.create!(ingredients)
+# puts 'Finished!'
+
+# puts ingredients
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+
+puts 'Finished!'
